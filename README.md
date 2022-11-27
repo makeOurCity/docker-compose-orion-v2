e8cbf21deba72b083a12677ec460bb1e8cb7a233
Ushio Shugo <ushio.s@gmail.com>
2022-11-27 17:25:05 +0900

    Add scripts file

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # docker-compose-orionv2
 Launch FIWARE-Orion v2 in local
+
+# Orionの起動
+
+```console
+$ docker compose up
+```
+
+# 各種スクリプトの起動
+
+```console
+$ npm install
+```
---
+++ src/01_create.js
@@ -0,0 +1,19 @@
+const axios = require("axios");
+
+const data = {
+  id: "urn:ngsi-ld:MEASUREMENT:id:1",
+  type: "DeviceMeasurement",
+  numValue: {
+    type: "Number",
+    value: 20.5,
+  },
+};
+
+axios
+  .post("http://localhost:1026/v2/entities", data)
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
